12e48949a2e3426bd2ba3619f7fdac7aaab3b4f3
Nam Pham <nam.phho@gmail.com>
2019-08-01 00:21:38 +0700

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>Revents Hnam vs Bao Ngoc</h1>
+      </div>
+    ); 
+  }
+}
+
+
+export default App;
