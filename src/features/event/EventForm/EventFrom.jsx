f3980ebb603ff0241ab85859070886f2f772325e
Nam Pham <nam.phho@gmail.com>
2019-08-30 13:19:43 +0700

    router

--- src/features/event/EventForm/EventFrom.jsx
+++ src/features/event/EventForm/EventFrom.jsx
@@ -1,33 +1,32 @@
 import React, { Component } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
-
+import { Link } from "react-router-dom";
 
 class EventFrom extends Component {
   state = {
-    title: '',
-    date: '',
-    city: '',
-    venue: '',
-    hosted: ''
-  }
+    title: "",
+    date: "",
+    city: "",
+    venue: "",
+    hostedBy: ""
+  };
 
-  componentDidMount(){
-    if (this.props.selectedEvent !== null){
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
       this.setState({
-        ...this.props.selectedEvent,
-        hosted : this.props.selectedEvent.hostedBy
+        ...this.props.selectedEvent
       });
     }
   }
 
-  handleFormSubmit = (evt) => {
+  handleFormSubmit = evt => {
     evt.preventDefault();
     if (this.state.id) {
-      this.props.updateEvent(this.state)
+      this.props.updateEvent(this.state);
     } else {
-      this.props.createEvent(this.state)  
+      this.props.createEvent(this.state);
     }
-  }
+  };
 
   // handleInputChange = (evt) => {
   //   this.setState({
@@ -36,44 +35,73 @@ class EventFrom extends Component {
   // }
 
   //destructure event
-  handleInputChange = ({target: {name, value}}) => {
+  handleInputChange = ({ target: { name, value } }) => {
     this.setState({
       [name]: value
-    })
-  }
-
-
+    });
+  };
 
   render() {
-  const {cancelFormOpen} = this.props;
-    const {title, date, city, venue, hosted} = this.state;
+    const { cancelFormOpen } = this.props;
+    const { title, date, city, venue, hostedBy } = this.state;
     return (
       <Segment>
-        <Form onSubmit={this.handleFormSubmit} autoComplete='off'>
+        <Form onSubmit={this.handleFormSubmit} autoComplete="off">
           <Form.Field>
             <label>Event Title</label>
-            <input name="title" onChange={this.handleInputChange} value={title} placeholder="Event Title" />
+            <input
+              name="title"
+              onChange={this.handleInputChange}
+              value={title}
+              placeholder="Event Title"
+            />
           </Form.Field>
           <Form.Field>
             <label>Event Date</label>
-            <input name="date" onChange={this.handleInputChange} value = {date} type="date" placeholder="Event Date" />
+            <input
+              name="date"
+              onChange={this.handleInputChange}
+              value={date}
+              type="date"
+              placeholder="Event Date"
+            />
           </Form.Field>
           <Form.Field>
             <label>City</label>
-            <input name="city" onChange={this.handleInputChange} value = {city} placeholder="City event is taking place" />
+            <input
+              name="city"
+              onChange={this.handleInputChange}
+              value={city}
+              placeholder="City event is taking place"
+            />
           </Form.Field>
           <Form.Field>
             <label>Venue</label>
-            <input name="venue" onChange={this.handleInputChange} value = {venue} placeholder="Enter the Venue of the event" />
+            <input
+              name="venue"
+              onChange={this.handleInputChange}
+              value={venue}
+              placeholder="Enter the Venue of the event"
+            />
           </Form.Field>
           <Form.Field>
             <label>Hosted By</label>
-            <input name="hosted" onChange={this.handleInputChange} value = {hosted} placeholder="Enter the name of person hosting" />
+            <input
+              name="hosted"
+              onChange={this.handleInputChange}
+              value={hostedBy}
+              placeholder="Enter the name of person hosting"
+            />
           </Form.Field>
           <Button positive type="submit">
             Submit
           </Button>
-          <Button type="button" onClick={cancelFormOpen}>Cancel</Button>
+          <Button
+            type="button"
+            onClick={cancelFormOpen}
+          >
+            Cancel
+          </Button>
         </Form>
       </Segment>
     );
---
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingsDashboard = () => {
+    return (
+        <div>
+            <h1>Settings DashBoard</h1>
+        </div>
+    )
+}
+
+export default SettingsDashboard
