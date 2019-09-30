9000c6ad7b89f8f8ca2cca10cc8cb4f2c12e2af4
Nam Pham <nam.phho@gmail.com>
2019-09-30 23:58:23 +0700

    config add GOOGLE API KEY

--- src/features/testarea/SimpleMap.jsx
+++ src/features/testarea/SimpleMap.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Icon } from 'semantic-ui-react';
+import { GOOGLE_MAP_API } from '../../app/appConstants';
 
 const AnyReactComponent = () => <Icon name='marker' size='big' color='red'/>;
 
@@ -14,7 +15,7 @@ class SimpleMap extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '300px', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "abc"}}
+          bootstrapURLKeys={{ key: GOOGLE_MAP_API}}
           defaultCenter={latlng}
           defaultZoom={this.props.zoom}
         >
